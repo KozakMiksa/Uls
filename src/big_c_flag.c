ba6fb5707951b57e28d754f4eda0515aa706f288
KozakMiksa <kozak.miksa@gmail.com>
2020-02-14 17:53:24 +0200

    -m, with bug /

--- src/big_c_flag.c
+++ src/big_c_flag.c
@@ -17,17 +17,15 @@ static int longest_name(t_list *list) {
     return lenght;
 }
 
-void mx_big_c_flag(t_list *files, t_list *flags) {
-    struct winsize ws;
+void mx_big_c_flag(t_list *files, t_list *flags, int ws_size) {
     int size_list = mx_list_size(files);
     int lenght = longest_name(files);
     char flag = mx_get_flag(flags, "ftS", 'C');
 
     mx_select_sort(&files, flags, flag);
-    ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
     if (mx_get_flag(flags, "r", 'C') == 'r')
         mx_r_sort(&files);
-    mx_mult_colum(files, ws.ws_col, lenght, size_list);
+    mx_mult_colum(files, ws_size, lenght, size_list);
     while (files != NULL)
         mx_pop_front(&files);
 }
--- src/m_flag.c
+++ src/m_flag.c
@@ -1,14 +1,28 @@
 #include "uls.h"
 
-void mx_m_flag(t_list *files, t_list *flags) {
+void mx_m_flag(t_list *files, t_list *flags, int ws_size) {
     char flag = mx_get_flag(flags, "ftS", 'C');
+    int check = 0;
+    int tmp = 0;
 
     mx_select_sort(&files, flags, flag);
     if (mx_get_flag(flags, "r", 'C') == 'r')
         mx_r_sort(&files);
     while (files != NULL) {
-        mx_printstr(files->data);
-        mx_printstr(", ");
+        tmp = mx_strlen(files->data) + 2;
+        check =  check + tmp;
+        if (check < ws_size) {
+            mx_printstr(files->data);
+        }
+        else {
+        	check = tmp;
+            mx_printchar('\n');
+            mx_printstr(files->data);
+        }
+        if (files->next != NULL)
+            mx_printstr(", ");
+
         files = files->next;
     }
+    mx_printchar('\n');
 }
