7f6abc1608bcbaaa569ffafdfd6ce764a9bd5332
kruzhanska <kamilla.ruzhanska@gmail.com>
2020-03-05 19:23:48 +0200

    true sort

--- src/ascii_sort.c
+++ src/ascii_sort.c
@@ -4,12 +4,14 @@ void mx_ascii_sort(t_list **l) {
     char *tmp = NULL;
     t_list *list = *l;
 
-    for (t_list *i = list; i->next != NULL; i = i->next) {
-        for (t_list *j = list; j->next != NULL; j = j->next) {
-            if (mx_strcmp(j->data, j->next->data) > 0) {
-            	tmp = j->data;
-            	j->data = j->next->data;
-            	j->next->data = tmp;
+    if (*l != NULL) {
+        for (t_list *i = list; i->next != NULL; i = i->next) {
+            for (t_list *j = list; j->next != NULL; j = j->next) {
+                if (mx_strcmp(j->data, j->next->data) > 0) {
+                    tmp = j->data;
+                    j->data = j->next->data;
+                    j->next->data = tmp;
+                }
             }
         }
     }
